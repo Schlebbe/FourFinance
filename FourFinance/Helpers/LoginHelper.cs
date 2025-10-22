92b25d67e9d7140d8ecdb7960756e965aefdecf0
sebastian@temtron.se <sebastian@temtron.se>
2025-10-22 16:37:12 +0200

    Add ASCII "art" to welcome screen

--- FourFinance/Helpers/LoginHelper.cs
+++ FourFinance/Helpers/LoginHelper.cs
@@ -6,18 +6,21 @@ namespace FourFinance.Helpers
     {
         public static void LoginPrompt()
         {
+            PrintAsciiWelcome();
+
             // Using Spectre.Console to create a simple login prompt
             var choice = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
-                    .Title("Welcome to [green]FourFinance[/]")
                     .PageSize(3)
                     .AddChoices(new[] {
-                        "Login", "Exit"
+                            "Login", "Exit"
                     }));
+
+            AnsiConsole.Clear();
 
             if (choice == "Exit")
             {
-                AnsiConsole.WriteLine($"Thank you for visiting [green]FourFinance[/]!");
+                AnsiConsole.Markup($"Thank you for visiting [green]FourFinance[/]!");
                 Environment.Exit(0);
             }
 
@@ -36,12 +39,59 @@ namespace FourFinance.Helpers
 
             if (user == null)
             {
-                AnsiConsole.WriteLine("Invalid username/email or password. Please try again.");
+                AnsiConsole.Markup("Invalid username/email or password. Please try again.");
                 Login(); // Retry login
                 return;
             }
 
-            AnsiConsole.WriteLine($"Welcome back, {user.Name}!");
+            AnsiConsole.Markup($"Welcome back, [blue]{user.Name}[/]!");
+        }
+
+        private static void PrintAsciiWelcome()
+        {
+            AnsiConsole.Clear();
+
+            var art = @"
+       [blue]__        __   _  [/]                                      
+       [blue]\ \      / /__| | ___ ___  _ __ ___   ___[/] 
+        [blue]\ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \[/] 
+         [blue]\ V  V /  __/ | (_| (_) | | | | | |  __/[/] 
+          [blue]\_/\_/ \___|_|\___\___/|_| |_| |_|\___[/] 
+
+          .---------------------------------------.      .----------------.
+         /  ____________[deeppink4_2]BANK[/] OF [green]FOURFINANCE[/]______  \    /  .------------.  \
+        /  /  ||  ||  ||  ||  ||  ||  ||  ||  ||  \  \  /  /  ________   \  \
+       |  |   ||  ||  ||  ||  ||  ||  ||  ||  ||   |  | |  |  |[yellow]$$$$$$[/]| |  | |
+       |  |   ||  ||  ||  ||  ||  ||  ||  ||  ||   |  | |  |  |[yellow]$$$$$$[/]| |  | |
+       |  |   ||  ||  ||  ||  ||  ||  ||  ||  ||   |  | |  |  |[yellow]$$$$$$[/]| |  | |
+       |  |   ||  ||  ||  ||  ||  ||  ||  ||  ||   |  | |  |  |[yellow]$$$$$$[/]| |  | |
+       |  |   ||  ||  ||  ||  ||  ||  ||  ||  ||   |  | |  |  |[yellow]$$$$$$[/]| |  | |
+        \  \______________________________________/  /   \  \  '------'  /  /
+         '-------------------------------------------'     '--------------'
+           [yellow]__ .--. _[/]
+       [yellow].-;.-""-.-;`_;-,[/]
+     [yellow].(_( `)-;___),-;_),[/]
+    [yellow](.( `\.-._)-.(   ). )[/]
+  ,[yellow](_`'--;.__\  _).;--'`_)[/]
+ // [yellow])[/]`--..__ ``` _[yellow]( o )'('[/];.
+ \;'        `````  `\\   '.\\
+ /                   ':.___//
+|                      '---'|
+;                           ;
+ \                         /
+  '.                     .'
+    '-,.__         __.,-'
+     (___/`````````\___)
+    ";
+
+            // Render each line to allow Spectre.Console markup (colors) to apply properly.
+            var lines = art.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                AnsiConsole.MarkupLine(line);
+            }
+
+            //AnsiConsole.WriteLine("");
         }
     }
 }
