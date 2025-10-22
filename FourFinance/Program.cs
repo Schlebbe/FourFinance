ef392e79e8040be751ab36bcc84fd1bc193af9ac
sebastian@temtron.se <sebastian@temtron.se>
2025-10-22 15:30:04 +0200

    Added dummy data generator

--- FourFinance/Program.cs
+++ FourFinance/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace FourFinance
+﻿using FourFinance.Helpers;
+
+namespace FourFinance
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DummyDataHelper.SeedDummyData(); // Seed some dummy users for testing
         }
     }
 }
