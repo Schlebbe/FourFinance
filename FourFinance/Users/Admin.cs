39e044632a921b4647dc163119786433f2e743b6
sebastian@temtron.se <sebastian@temtron.se>
2025-10-22 12:30:14 +0200

    Add Admin, Customer and User class/interface

---
+++ FourFinance/Users/Admin.cs
@@ -0,0 +1,28 @@
+﻿namespace FourFinance.Users
+{
+    public class Admin : IUser
+    {
+        public Guid Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+        public Admin(Guid id, int age, string name, string email, string password, string userName)
+        {
+            Id = id;
+            Age = age;
+            Name = name;
+            Email = email;
+            Password = password;
+            UserName = userName;
+            Id = Guid.NewGuid();
+        }
+
+        public void AddCustomer()
+        {
+            //TODO: Add customer to management system list
+        }
+    }
+}
