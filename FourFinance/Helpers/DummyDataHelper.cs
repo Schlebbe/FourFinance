6fa052a9ee8862a6ede4df1a0d2fa698d7f1b40b
sebastian@temtron.se <sebastian@temtron.se>
2025-10-23 13:11:25 +0200

    Add currency to constructor and remove guid from dummydata

--- FourFinance/Helpers/DummyDataHelper.cs
+++ FourFinance/Helpers/DummyDataHelper.cs
@@ -6,11 +6,11 @@ namespace FourFinance.Helpers
     {
         public static void SeedDummyData()
         {
-            BankHelper.AddUser(new Admin(Guid.NewGuid(), 23, "Admin User", "AdminUser@example.com", "adminpass", "AdminUser"));
-            BankHelper.AddUser(new Admin(Guid.NewGuid(), 82, "Admin Too", "AdminToo@example.com", "adminpass", "AdminToo"));
-            BankHelper.AddUser(new Admin(Guid.NewGuid(), 32, "Admin Also", "AdminAlso@example.com", "adminpass", "AdminAlso"));
-            BankHelper.AddUser(new Admin(Guid.NewGuid(), 46, "Admin Adminsson", "AdminAdminsson@example.com", "adminpass", "AdminAdminsson"));
-            BankHelper.AddUser(new Admin(Guid.NewGuid(), 30, "Admin Adminsdotter", "AdminAdminsdotter@example.com", "adminpass", "AdminAdminsdotter"));
+            BankHelper.AddUser(new Admin(23, "Admin User", "AdminUser@example.com", "adminpass", "AdminUser"));
+            BankHelper.AddUser(new Admin(82, "Admin Too", "AdminToo@example.com", "adminpass", "AdminToo"));
+            BankHelper.AddUser(new Admin(32, "Admin Also", "AdminAlso@example.com", "adminpass", "AdminAlso"));
+            BankHelper.AddUser(new Admin(46, "Admin Adminsson", "AdminAdminsson@example.com", "adminpass", "AdminAdminsson"));
+            BankHelper.AddUser(new Admin(30, "Admin Adminsdotter", "AdminAdminsdotter@example.com", "adminpass", "AdminAdminsdotter"));
 
             BankHelper.AddUser(new Customer(25, "John Doe", "JohnDoe@example.com", "password123", "JohnDoe"));
             BankHelper.AddUser(new Customer(40, "Jane Smith", "JaneSmith@example.com", "securepass", "JaneSmith"));
