7bde7e7cb84be20ca9a5c15fbf164f125e4c52a6
sebastian@temtron.se <sebastian@temtron.se>
2025-11-06 15:28:07 +0100

    Renamed transactionhelper to SchedulerHelper

---
+++ FourFinance/Helpers/SchedulerHelper.cs
@@ -0,0 +1,67 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourFinance.Helpers
+{
+    public static class SchedulerHelper
+    {
+        public static readonly ConcurrentQueue<Func<Task>> pendingTransactions = new ConcurrentQueue<Func<Task>>();
+        public static CancellationTokenSource cts = new CancellationTokenSource();
+
+        public static async Task StartTransactionSchedulerAsync(CancellationToken token) 
+        {
+            while (!token.IsCancellationRequested) 
+            {
+                await Task.Delay(TimeSpan.FromSeconds(15), token);
+
+                while (pendingTransactions.TryDequeue(out var action)) 
+                {
+                    try
+                    {
+                        await Program.ConsoleLock.WaitAsync(token);
+                        await action();
+                    }
+                    catch (Exception ex) 
+                    {
+                        AnsiConsole.MarkupLine($"Error running action: {ex.Message}");
+                    }
+                    finally
+                    {
+                        Program.ConsoleLock.Release();
+                    }
+                }
+            }
+        }
+
+        public static async Task StartInterestSchedulerAsync(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(15), token);
+
+                try
+                {
+                    await Program.ConsoleLock.WaitAsync(token);
+                    var savingsAccounts = BankHelper.GetAllSavingsAccounts();
+                    foreach (var account in savingsAccounts)
+                    {
+                        account.CalculateAndApplyInterest();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AnsiConsole.MarkupLine($"Error calculating interest: {ex.Message}");
+                }
+                finally
+                {
+                    Program.ConsoleLock.Release();
+                }
+            }
+        }
+    }
+}
